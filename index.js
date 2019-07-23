54bfb6f0c134d22026ce4e7e23650eeea20c19f4
Joberth Rogers <joberth.rogers18@gmail.com>
2019-07-23 16:36:05 -0400

    Adding an engine and fix an error

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const path = require('path');
+
+//configurando engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.send(<h1>Let's hacking guys!!!</h1>);
+    res.render('index', {
+        title: 'My first deploy continous',
+        version: '0.0.1'
+    });
 });
 
 app.listen(PORT, () => {
