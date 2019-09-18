1ca50959e00301a3c8f7a2b666735f5a6faaf711
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-17 21:49:46 -0400

    Added viewset and routers

--- ticketapp/ticketapp/urls.py
+++ ticketapp/ticketapp/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.contrib.auth.models import User
+from ticket.models import Category, Ticket
+from rest_framework import routers
+
+from ticket.views import UserViewSet, TicketViewSet, CategoryViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'api/users', UserViewSet)
+
+router.register(r'api/tickets', TicketViewSet)
+
+router.register(r'api/category', CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include(router.urls)),
+    path(r'api/', include('rest_framework.urls', namespace='rest_framework'))   
 ]
