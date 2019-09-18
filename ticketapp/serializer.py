1f344895255454c0a0016714d01f4fa49dc33846
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-17 23:04:24 -0400

    Change to only modelserializer

--- ticketapp/serializer.py
+++ ticketapp/serializer.py
@@ -1,37 +1,22 @@
 from rest_framework import serializers
-
 from django.contrib.auth.models import User
 from ticket.models import Ticket, Category
 
-# Serializer define the api representation
-class UserSerializer (serializers.Serializer):
-  id = serializers.IntegerField()
-  username = serializers.CharField(max_length=200)
-  password = serializers.CharField(max_length=100, style={"input_type": "password"})
-  is_staff = serializers.BooleanField(default=False)
 
-  def create(self, validated_data):
+# Serializers define the API representation.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_staff')
+        
+# Serializers define the API representation.
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ('id','title', 'ticket_id','user', 'status','content', 'category','created', 'modified')
 
-    return User.objects.create(**validated_data)
-
-  def update(self, instance, validated_data):
-
-    instance.username = validated_data.get('username', instance.username)
-    instance.password = validated_data.get('password', instance.password)
-    instance.is_staff = validated_data.get('is_staff', instance.is_staff)
-    instance.save()
-    return instance
-
-  class Meta:
-    model = User
-    fields = ('id','username', 'password', 'is_staff')
-
-class TicketSerializer (serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = Ticket
-    fields = ('id', 'title', 'ticket_id', 'user', 'content', 'category', 'created', 'modified')
-
-class CategorySerializer (serializers.ModelSerializer):
-  class Meta:
-    model = Category
-    fields = ('name', 'slug')
+# Serializers define the API representation.
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id','name', 'slug')
